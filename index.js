5a3d3c683379316de65af01449178ffd34f0e313
fmnijk <fmnijk@gmail.com>
2020-12-27 17:20:57 +0800

    make it clear

--- index.js
+++
@@ -1,9 +0,0 @@
-function copyToClipboard(id){
-    var from = document.getElementById(id);
-    var range = document.createRange();
-    window.getSelection().removeAllRanges();
-    range.selectNode(from);
-    window.getSelection().addRange(range);
-    document.execCommand('copy');
-    window.getSelection().removeAllRanges();
-}
